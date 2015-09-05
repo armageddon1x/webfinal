addc1a8518dab28941b9a50ea734e0900f4a0c27
armageddon <armageddon1x@gmail.com>
2015-09-05 12:13:51 -0400

    completed turn loop, added all attack variables

--- js/function.js
+++ js/function.js
@@ -20,7 +20,8 @@ $(document).ready(function(){
 		var gameUpdate = setTimeout(function(){
 
 			console.log( "checking...");
-			magecraft();
+			menu();
+			submenu();
 			dragon();
 
 			if( HP_enemy <= 0 ){
@@ -31,7 +32,7 @@ $(document).ready(function(){
 				game();
 			}
 
-		}, 5500)
+		}, 5000)
 	}
 
 	game();
@@ -68,24 +69,201 @@ $(document).ready(function(){
 	var air_mana=5;
 	var earth_mana=5;
 
+	//battojutsu attack values
+	var inazama=50;
+	var shuntensatsu=50;
+	var iaido=50;
+	var ameterasu=50;
+
+	//mana values
+	var inazama_mana=5;
+	var shuntensatsu_mana=5;
+	var iaido_mana=5;
+	var ameterasu_mana=5;
+
+	//special attack values
+	var swordslash=50;
+	var bladebarrage=50;
+	var pierce=50;
+	var cutter=50;
+
+	//mana values
+	var swordslash_mana=5;
+	var bladebarrage_mana=5;
+	var pierce_mana=5;
+	var cutter_mana=5;
+
+	//program attack values
+	var steelpunch=50;
+	var platinumslash=50;
+	var titaniumblow=50;
+	var goldensmash=50;
+
+	//mana values
+	var steelpunch_mana=5;
+	var platinumslash_mana=5;
+	var titaniumblow_mana=5;
+	var goldensmash_mana=5;
+
+	//counter for menu cycling
+	var i=0;
+
+	function menu(){
+		//
+
+		if(i===0){
+			console.log("0");
+			i=1;
+			//removes the none class from the first menu
+			$("#options").removeClass("none");
+		}else if (i===1){
+			console.log("1");
+			
+			//removes the first menu
+			$("#options").addClass("none");
+			//displays the skills menu
+			$("#magic").removeClass("none");
+			//click on skills menu to make attack
+			$("#magic").click(function(){
+				i=2;
+
+			//this makes the skills menu disappear
+			$("#magic").addClass("none");
+			//this makes the next menu appear
+			$("#options2").removeClass("none");
+		});
+
+		}else if(i===2){
+			console.log("2");
+			
+
+			//removes second menu
+			$("#options2").addClass("none");
+			//displays the skills menu
+			$("#batto").removeClass("none");
+			//click on skills menu to make attack
+			$("#batto").click(function(){
+				i=3;
+			//this makes the skills menu disappear
+			$("#batto").addClass("none");
+			//this makes the next menu appear
+			$("#options3").removeClass("none");
+		});
+		}else if(i===3){
+			console.log("3");
+			
+
+			//removes third menu
+			$("#options3").addClass("none");
+			//displays the skills menu
+			$("#special").removeClass("none");
+			//click on skills menu to make attack
+			$("#special").click(function(){
+				i=4;
+			//this makes the skills menu disappear
+			$("#special").addClass("none");
+			//this makes the next menu appear
+			$("#options4").removeClass("none");
+			});
+		}else{
+			console.log("4");
+			
+			//removes fourth menu
+			$("#options4").addClass("none");
+			//displays the skills menu
+			$("#program").removeClass("none");
+			//click on skills menu to make attack
+			$("#program").click(function(){
+				i=1;
+			//this makes the skills menu disappear
+			$("#program").addClass("none");
+			//this makes the first menu appear again
+			$("#options").removeClass("none");
+			});
+		};
+
+
+ 		//makes magic menu appear
+		// $("#magic").removeClass("none");
+
+		// //click on magic menu
+		// $("#magic").click(function(){
+		// 	//this makes magic menu disappear
+		// 	$("#magic").addClass("none");
+		// });
+		// if (i===1){
+		// 	console.log("i before equal "+i);
+		// 	i=2;
+		// 	console.log("i equal "+i);
+
+		// 	//makes magic menu appear
+		// 	$("#magic").removeClass("none");
+		// 	//click on magic menu
+		// 	$("#magic").click(function(){
+		// 		//this makes magic menu disappear
+		// 		$("#magic").addClass("none");
+		// 		$("#options").addClass("none");
+		// 		$("#options2").removeClass("none");
+		// 	});
+
+		// }else if(i===2){
+		// 	console.log("i before equal "+i);
+		// 	i=3;
+		// 	console.log("i equal "+i);
+
+		// 	//click on magic menu
+		// 	$("#battojutsu").click(function(){
+		// 		//this makes magic menu disappear
+		// 		$("#batto").addClass("none");
+		// 		$("#options2").addClass("none");
+		// 		$("#options").removeClass("none");
+		// 	});
+
+		// }else if (i===3){
+		// 	console.log("i before equal "+i);
+		// 	i=4;
+		// 	console.log("i equal "+i);
+
+		// 	//click on magic menu
+		// 	$("#special").click(function(){
+		// 		//this makes magic menu disappear
+		// 		$("#special").addClass("none");
+		// 		$("#options3").addClass("none");
+		// 		$("#options4").removeClass("none");
+		// 	});
+
+		// }else{
+		// 	console.log("i before equal "+i);
+		// 	i=1;
+		// 	console.log("i equal "+i);
+
+		// 	$("#program").removeClass("none");
+		// 	//click on magic menu
+		// 	$("#program").click(function(){
+		// 		//this makes magic menu disappear
+		// 		$("#program").addClass("none");
+		// 		$("#options4").addClass("none");
+		// 		$("#options").removeClass("none");
+		// 	});
+		// };
+
+		//
+	};
+
 	//clicking first on the magic selector and then click on the spell to use
-	function magecraft(){
+	function submenu(){
+		//selects the second child from the options menu since those lead to 
+		//another menu of skills  (ex: #magecraft -> #magic)
 		$("ul li:nth-child(2)").click(function(){
 
 		//makes each magic click only happen once
-		$("#magic li").unbind('click');
+		$(".submenu li").unbind('click');
 
-		//makes magic menu appear
-		$("#magic").removeClass("none");
-
-		//click on magic menu
-		$("#magic").click(function(){
-			//this makes magic menu disappear
-			$("#magic").addClass("none");
-		});
+		//runs menu function for menu cycling
+		menu();
 
 		//this get the attack name once you click on the attack
-		$("#magic li").click(function(){
+		$(".submenu li").click(function(){
 			var attack_name=$(this).attr("id");
 			console.log(attack_name)
 
@@ -115,6 +293,8 @@ $(document).ready(function(){
 			//update HP and MP meters
 			$("#MP_ally").html(MP_ally);
 			$("#HP_enemy").html(HP_enemy);
+
+			//progress bar for meters
 			progress(MP_ally, $('#MP_ally_progress_bar'));
 			progress(((HP_enemy*100)/HP_enemy_max), $("#HP_enemy_progress_bar"));
 
@@ -131,22 +311,34 @@ $(document).ready(function(){
 	function dragon(){
 		//beginning
 
-		var dark_purge=50;			//selector numbers will be 0,1,2,3
-		var starfall=75;				//4,5,6
-		var grand_cross=100;		//7,8
-		var armageddon=200;			//9
+		//dragon attacks
+		var dark_purge=50;			//selector numbers will be 0,1,2,3,4,5,6
+		var starfall=75;				//7,8,9,10,11
+		var grand_cross=100;		//12,13,14
+		var armageddon=200;			//15
 
-		var x = Math.floor((Math.random() * 10));
+		//dragon mana costs
+		var dark_purge=50;
+		var starfall=75;
+		var grand_cross=100;
+		var armageddon=200;
+
+		//selects random number for launching attack
+		var x = Math.floor((Math.random() * 15));
 		console.log(x);
 
-		if (x<4){
-			alert("death purge")
-		}else if(x<7&&x>3){
-			alert("starfall")
-		}else if(x===7||x===8){
-			alert("grand cross")
+		//dark purge from 0-6
+		if (x<7){
+			console.log("dark purge")
+		//starfall from 7-11
+		}else if(x>6&&x<12){
+			console.log("starfall")
+		//grand cross from 12-14
+		}else if(x>11&&x<15){
+			console.log("grand cross")
+		//armageddon is 15
 		}else{
-			alert("armageddon")
+			console.log("armageddon")
 		};
 
 		//end
@@ -196,18 +388,18 @@ $(document).ready(function(){
 	}
 
 	//audio loop
-	var audio_file = new Audio('sounds/bgm.mp3');
-	audio_file.play();
-	audio_file.addEventListener('timeupdate', function(){
-  var buffer = 4
-  if(this.currentTime>(this.duration-buffer)){
-      this.currentTime = 25
-      this.play()
-      console.log(this.currentTime);
-      console.log(this.duration-buffer);
-      console.log(this.duration);
-      console.log(buffer);
-  }}, false);
+	// var audio_file = new Audio('sounds/bgm.mp3');
+	// audio_file.play();
+	// audio_file.addEventListener('timeupdate', function(){
+ //  var buffer = 4
+ //  if(this.currentTime>(this.duration-buffer)){
+ //      this.currentTime = 25
+ //      this.play()
+ //      console.log(this.currentTime);
+ //      console.log(this.duration-buffer);
+ //      console.log(this.duration);
+ //      console.log(buffer);
+ //  }}, false);
 
 	//end
 });
